2dba86e2b4bffcb4e8bc9daeb1513c73636e327e
N.Cheung <thisisaneon@gmail.com>
2026-05-10 15:53:05 +0800

    updates trees

--- 06Trees/01BSTree/OtherExercises.cpp
+++ 06Trees/01BSTree/OtherExercises.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 #include<queue>
 using namespace std;
 
@@ -233,3 +234,169 @@ public:
     }
 };
 // 明天建议学习带dummy的写法，逻辑会简单一些。
+
+/*
+
+
+                                ？？？
+
+
+*/
+
+
+
+class Connect2 {
+public:
+    Node* connect(Node* root) {
+        if (root == nullptr) { return root; }
+        queue<Node* >q;
+        q.push(root);
+        while (!q.empty()) {
+            const int n = q.size();
+            for (int i = 0; i < n; i++) {
+                auto front = q.front();
+                q.pop();
+                if (front->left) { q.push(front->left); }
+                if (front->right) { q.push(front->right); }
+                if (i == n - 1) { break; }
+                auto next = q.front();
+                front->next = next;
+            }
+        }
+        return root;
+    }
+};
+
+/*
+* 路径数字之和
+给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+每条从根节点到叶节点的路径都代表一个数字：
+
+例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+计算从根节点到叶节点生成的 所有数字之和 。
+
+叶节点 是指没有子节点的节点。
+*/
+class SumNumbers {
+public:
+    void DFS(TreeNode* node, int& sum, int num) {
+        if (node == nullptr) {
+            return;
+        }
+        if (node->left == nullptr && node->right == nullptr) {
+            sum += num * 10 + node->val;
+            return;
+        }
+        DFS(node->left, sum, num * 10 + node->val);
+        DFS(node->right, sum, num * 10 + node->val);
+    }
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        int num = 0;
+        DFS(root, sum, num);
+        return sum;
+    }
+};
+
+
+/*
+实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+int next()将指针向右移动，然后返回指针处的数字。
+注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+*/
+class BSTIterator {
+private:
+    // L V R
+    void DFS(vector<int>& nums, TreeNode* node) {
+        if (node == nullptr) {
+            return;
+        }
+        DFS(nums, node->left);
+        nums.push_back(node->val);
+        DFS(nums, node->right);
+    }
+    vector<int>nums;
+    int idx = 0;
+public:
+    BSTIterator(TreeNode* root) {
+        DFS(nums, root);
+    }
+    int next() {
+        return nums[idx++];
+    }
+    bool hasNext() {
+        int n = nums.size();
+        return idx < n;
+    }
+};
+// 写另一种方法之前，我们先回顾一下迭代的写法
+// VLR
+void preOrder(TreeNode* root) {
+    if (root == nullptr) { return; }
+    stack<TreeNode* >s;
+    s.push(root);
+    while (!s.empty()) {
+        auto top = s.top();
+        cout << top->val << endl;
+        s.pop();
+        if (top->right) {
+            s.push(top->right);
+        }
+        if (top->left) {
+            s.push(top->left);
+        }
+    }
+}
+// LVR
+void inOrder(TreeNode* root) {
+    if (root == nullptr) { return; }
+    stack<TreeNode* > s;
+    TreeNode* node = root;
+    while (node != nullptr){
+        s.push(node);
+        node = node->left;
+    }
+    while (!s.empty()) {
+        auto top = s.top();
+        s.pop();
+        cout << top->val << endl;
+        if (top->right != nullptr) {
+            auto right = top->right;
+            while(right != nullptr){
+                s.push(right);
+                right = right->left;
+            }
+        }
+    }
+}
+// 好的我们开始重写这个题：
+class BSTIterator {
+private:
+    stack<TreeNode*>s;
+public:
+    BSTIterator(TreeNode* root) {
+        TreeNode* node = root;
+        while (node != nullptr){
+            s.push(node);
+            node = node->left;
+        }
+    }
+    int next() {
+        auto top = s.top();
+        s.pop();
+        if (top->right) {
+            TreeNode* right = top->right;
+            while (right) {
+                s.push(right);
+                right = right->left;
+            }
+        }
+        return top->val;
+    }
+    bool hasNext() {
+        return !s.empty();
+    }
+};
